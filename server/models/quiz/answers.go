b6cabadb95d9751cc1b57b9af532e65aa3969b54
Aglicheev Mikhail <Toba.ru@mail.ru>
2022-03-12 18:55:57 +0300

    Removed error-prone letter

--- server/models/quiz/answers.go
+++ server/models/quiz/answers.go
@@ -1,9 +1,9 @@
 package quiz
 
 const (
-	BASIC_TEST_YES    = "Возьмёшь её"
-	BASIC_TEST_NO     = "Убежишь и позовёшь родителей"
-	BASIC_TEST_MEXICO = "Перейдёшь границу с Мексикой"
+	BASIC_TEST_YES    = "Возьмешь её"
+	BASIC_TEST_NO     = "Убежишь и позовешь родителей"
+	BASIC_TEST_MEXICO = "Перейдешь границу с Мексикой"
 
 	BASIC_ANSWER_YES    = "Тебя поймал злодей"
 	BASIC_ANSWER_NO     = "Молодец"
