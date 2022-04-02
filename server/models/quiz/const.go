c05910f517a1c9b5d56e5f4b2cc508b4504ce9f2
Яшуваев Наум Исакович <yashuvaev.naum@wildberries.ru>
2022-04-02 19:08:47 +0300

    added helper, new cases are processored

--- server/models/quiz/const.go
+++ server/models/quiz/const.go
@@ -4,10 +4,14 @@ const (
 	MsgIncorrectInput = "Не смогла распознать твой ответ. Попробуй ещё раз."
 	MsgSelectQuiz     = "Выбери, какой тест хочешь пройти."
 	MsgQuestionRepeat = "Повторяю"
+	MsgStartOverTest  = "Хорошо, начнём тест сначала."
 )
 
 const (
-	QuizRootID = 0
+	QuizGetHelp       = -3
+	QuizFirstQuestion = -2
+	QuizQuitGame      = -1
+	QuizRootID        = 0
 )
 
 var (
